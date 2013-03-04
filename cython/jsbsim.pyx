8078fd7c19908dfb06e96c7b530de96ca3ecf73f
jgoppert <james.goppert@gmail.com>
2013-03-03 23:11:41 -0500

    Added most functions and docstrings for FGFDMExec.

--- cython/jsbsim.pyx
+++ cython/jsbsim.pyx
@@ -5,8 +5,8 @@ cdef extern from "JSBSim/FGFDMExec.h" namespace "JSBSim":
     cdef cppclass c_FGFDMExec "JSBSim::FGFDMExec":
         c_FGFDMExec(int root, int fdmctr)
         void Unbind()
-        void Run()
-        void RunIC()
+        bool Run()
+        bool RunIC()
         bool LoadModel(string model,
                        bool add_model_to_path)
         bool LoadModel(string aircraft_path,
@@ -82,41 +82,359 @@ cdef class FGFDMExec:
                 self.get_systems_path())
 
     def run(self):
-        self.thisptr.Run()
+        """
+        This function executes each scheduled model in succession.
+        @param return true if successful, false if sim should be ended
+        """
+        return self.thisptr.Run()
 
     def run_ic(self):
-        self.thisptr.RunIC()
+        """
+        Initializes the sim from the initial condition object and executes
+        each scheduled model without integrating i.e. dt=0.
+        @param return true if successful
+        """
+        return  self.thisptr.RunIC()
 
     def load_model(self, model, add_model_to_path = True):
+        """
+        Loads an aircraft model.
+        @param AircraftPath path to the aircraft/ directory. For instance:
+            "aircraft". Under aircraft, then, would be directories for various
+            modeled aircraft such as C172/, x15/, etc.
+        @param EnginePath path to the directory under which engine config
+            files are kept, for instance "engine"
+        @param SystemsPath path to the directory under which systems config
+            files are kept, for instance "systems"
+        @param model the name of the aircraft model itself. This file will
+            be looked for in the directory specified in the AircraftPath variable,
+            and in turn under the directory with the same name as the model. For
+            instance: "aircraft/x15/x15.xml"
+        @param addModelToPath set to true to add the model name to the
+            AircraftPath, defaults to true
+        @return true if successful 
+        """
         return self.thisptr.LoadModel(model, add_model_to_path)
 
     def load_model(self, model, aircraft_path,
                    engine_path, systems_path, add_model_to_path = True):
+        """
+        Loads an aircraft model.  The paths to the aircraft and engine
+        config file directories must be set prior to calling this.  See
+        below.
+        @param model the name of the aircraft model itself. This file will
+            be looked for in the directory specified in the AircraftPath variable,
+            and in turn under the directory with the same name as the model. For
+            instance: "aircraft/x15/x15.xml"
+        @param addModelToPath set to true to add the model name to the
+            AircraftPath, defaults to true
+        @return true if successful
+        """
         return self.thisptr.LoadModel(model, add_model_to_path)
 
+    def load_script(script, delta_t, initfile):
+        """
+        Loads a script
+        @param Script The full path name and file name for the script to be loaded.
+        @param deltaT The simulation integration step size, if given.  If no value is supplied
+            then 0.0 is used and the value is expected to be supplied in
+            the script file itself.
+        @param initfile The initialization file that will override the initialization file
+            specified in the script file. If no file name is given on the command line,
+            the file specified in the script will be used. If an initialization file 
+            is not given in either place, an error will result.
+        @return true if successfully loads; false otherwise. */
+        """
+        return self.thisptr.LoadModel(script, delta_t, initfile)
+
     def set_engine_path(self, path):
+        """
+        Sets the path to the engine config file directories.
+        @param path path to the directory under which engine config
+            files are kept, for instance "engine"
+        """
         return self.thisptr.SetEnginePath(path)
 
     def set_aircraft_path(self, path):
+        """
+        Sets the path to the aircraft config file directories.
+        @param path path to the aircraft directory. For instance:
+            "aircraft". Under aircraft, then, would be directories for various
+            modeled aircraft such as C172/, x15/, etc. 
+        """
         return self.thisptr.SetAircraftPath(path)
 
     def set_systems_path(self, path):
+        """
+        Sets the path to the systems config file directories.
+        @param path path to the directory under which systems config
+            files are kept, for instance "systems"
+        """
         return self.thisptr.SetSystemsPath(path)
 
     def set_root_dir(self, path):
+        """
+        Sets the root directory where JSBSim starts looking for its system directories.
+        @param rootDir the string containing the root directory.
+        """
         self.thisptr.SetRootDir(path)
 
     def get_engine_path(self):
+        """
+        Retrieves the engine path
+        """
         return self.thisptr.GetEnginePath()
 
     def get_aircraft_path(self):
+        """
+        Retrieves the aircraft path
+        """
         return self.thisptr.GetAircraftPath()
 
     def get_systems_path(self):
+        """
+        Retrieves the systems path
+        """
         return self.thisptr.GetSystemsPath()
 
+    def get_full_aircraft_path(self):
+        """
+        Retrieves the full aircraft path name
+        """
+        return self.thisptr.GetFullAircraftPath()
+
     def get_root_dir(self):
+        """
+        Retrieves the Root Directory.
+        @return the string representing the root (base) JSBSim directory.
+        """
         return self.thisptr.GetRootDir()
 
+    def get_property_value(self, name):
+        """
+        Retrieves the value of a property.
+        @param property the name of the property
+        @result the value of the specified property 
+        """
+        return self.thisptr.GetPropertyValue(name)
+
+    def set_property_value(self, name, value):
+        """
+        Sets a property value.
+        @param property the property to be set
+        @param value the value to set the property to *
+        """
+        self.thisptr.SetPropertyValue(name, value)
+
     def get_model_name(self):
+        """
+        Retrieves the model name.
+        """
         return self.thisptr.GetModelName()
+
+    def set_output_directive(self, fname):
+        """
+        Sets the output (logging) mechanism for this run.
+        Calling this function passes the name of an output directives file to
+        the FGOutput object associated with this run. The call to this function
+        should be made prior to loading an aircraft model. This call results in an
+        FGOutput object being built as the first Output object in the FDMExec-managed
+        list of Output objects that may be created for an aircraft model. If this call
+        is made after an aircraft model is loaded, there is no effect. Any Output
+        objects added by the aircraft model itself (in an &lt;output> element) will be
+        added after this one. Care should be taken not to refer to the same file
+        name.
+        An output directives file contains an &lt;output> &lt;/output> element, within
+        which should be specified the parameters or parameter groups that should
+        be logged.
+        @param fname the filename of an output directives file.
+        """
+        return self.thisptr.SetOutputDirectives(fname)
+
+    def force_output(self, index):
+        """
+        Forces the specified output object to print its items once
+        """
+        self.thisptr.ForceOutput(index)
+
+    def set_logging_rate(self, rate):
+        """
+        Sets the logging rate for all output objects (if any).
+        """
+        self.thisptr.SetLoggingRate(rate)
+
+    def get_output_filename(self):
+        """
+        Retrieves the current output filename.
+        @return the name of the output file for the first output specified by the flight model.
+            If none is specified, the empty string is returned.
+        """
+        return self.thisptr.GetOutputFileName()
+
+    def do_trim(self):
+        """
+        Executes trimming in the selected mode.
+        @param mode Specifies how to trim:
+            - tLongitudinal=0
+            - tFull
+            - tGround
+            - tPullup
+            - tCustom
+            - tTurn
+            - tNone
+        """
+        self.thisptr.DoTrim()
+
+    def do_simplex_trim(self):
+        """
+        Executes linearization with state-space output
+            * You must trim first to get an accurate state-space model
+        """
+        self.thisptr.DoSimplexTrim()
+
+    def do_linearization(self):
+        """
+        Executes linearization with state-space output
+            * You must trim first to get an accurate state-space model
+        """
+        self.thisptr.DoLinearization()
+
+    def do_disable_output(self):
+        """
+        Disables data logging to all outputs.
+        """
+        self.thisptr.DisableOutput()
+
+    def do_enable_output(self):
+        """
+        Enables data logging to all outputs.
+        """
+        self.thisptr.EnableOutput()
+
+    def hold(self):
+        """
+        Pauses execution by preventing time from incrementing.
+        """
+        self.thisptr.Hold()
+
+    def enable_increment_then_hold(time_steps):
+        """
+        Turn on hold after increment
+        """
+        self.thisptr.EnableIncrementThenHold(time_steps)
+
+    def check_incremental_hold(self):
+        """
+        Checks if required to hold afer increment
+        """
+        self.thisptr.CheckIncrementalHold()
+
+    def resume(self):
+        """
+        Resumes execution from a "Hold".
+        """
+        self.thisptr.Resume()
+
+    def holding(self):
+        """
+        Returns true if the simulation is Holding (i.e. simulation time is not moving).
+        """
+        return self.thisptr.Holding()
+
+    def reset_to_initial_conditions(self):
+        """
+        Resets the initial conditions object and prepares the simulation to run again.
+        """
+        self.thisptr.ResetToInitialConditions()
+
+    def set_debug_level(self, level):
+        """
+        Sets the debug level.
+        """
+        self.thisptr.SetDebugLevel(level) 
+
+    def query_property_catalog(self, check):
+        """
+        Retrieves property or properties matching the supplied string.
+        A string is returned that contains a carriage return delimited list of all
+        strings in the property catalog that matches the supplied check string.
+        @param check The string to search for in the property catalog.
+        @return the carriage-return-delimited string containing all matching strings
+            in the catalog.
+        """
+        return self.thisptr.QueryPropertyCatalog(check)
+
+    def print_property_catalog(self):
+        """
+        Print the contents of the property catalog for the loaded aircraft.
+        """
+        self.thisptr.PrintPropertyCatalog()
+
+    def set_trim_status(self, status):
+        self.thisptr.SetTrimStatus(status)
+
+    def get_trim_status(self):
+        return self.thisptr.GetTrimStatus()
+
+    def get_propulsion_tank_report(self):
+        return self.thisptr.GetPropulsionTankReport()
+
+    def get_sim_time(self):
+        """
+        Returns the cumulative simulation time in seconds.
+        """
+        return self.thisptr.GetSimTime()
+
+    def get_delta_t(self):
+        """
+        Returns the simulation delta T.
+        """
+        return self.thisptr.GetDeltaT()
+
+    def suspend_integration(self):
+        """
+        Suspends the simulation and sets the delta T to zero.
+        """
+        self.thisptr.SuspendIntegration()
+
+    def resume_integration(self):
+        """
+        Resumes the simulation by resetting delta T to the correct value.
+        """
+        self.thisptr.ResumeIntegration()
+
+    def integration_suspended(self):
+        """
+        Returns the simulation suspension state.
+        @return true if suspended, false if executing
+        """
+        return self.thisptr.IntegrationSuspended()
+
+    def set_sim_time(self, time):
+        """
+        Sets the current sim time.
+        @param time the current time
+        @return the current simulation time.
+        """
+        return self.thisptr.Setsim_time(time)
+
+    def set_dt(self, delta_t):
+        """
+        Sets the integration time step for the simulation executive.
+        @param delta_t the time step in seconds.
+        """
+        self.thisptr.Setdt(dt)
+
+    def incr_time(self): 
+        """
+        Increments the simulation time if not in Holding mode. The Frame counter
+        is also incremented.
+        @return the new simulation time.
+        """
+        return self.thisptr.IncrTime()
+
+    def get_debug_level(self):
+        """
+        Retrieves the current debug level setting.
+        """
+        return self.thisptr.GetDebugLevel()
